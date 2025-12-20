a631d1d8ca0864f676fc340929235d00f5c184cf
crrashh1542 <1542@crrashh.com>
2025-12-20 17:21:11 +0800

    Synchronize codes from 'master' [Code sync: master -> main]
    This commit synchronizes codes from base Git branch 'rel' at
    8df9fc01230fc972c7ee3bbdf000260eccacedd8.
    
    Signed-off-by: crrashh1542 <1542@crrashh.com>

---
+++ astro.config.mjs
@@ -0,0 +1,28 @@
+import icon from 'astro-icon'
+
+import { defineConfig } from 'astro/config'
+import { fileURLToPath } from 'url'
+import { resolve } from 'path'
+
+// https://astro.build/config
+export default defineConfig({
+    integrations: [icon()],
+    vite: {
+        build: {
+            assetsInlineLimit: 6144,
+            rollupOptions: {
+                output: {
+                    hashCharacters: 'hex',
+                    assetFileNames: '_rudeus/[name]-[hash].[ext]',
+                    entryFileNames: '_rudeus/[name]-[hash].js',
+                    minifyInternalExports: true,
+                },
+            },
+        },
+        resolve: {
+            alias: {
+                '@': resolve(fileURLToPath(new URL('./src', import.meta.url))),
+            },
+        },
+    },
+})
---
+++ src/content/about.md
@@ -0,0 +1,2 @@
+# Hello, world!
+这是一个基于 Astro 的博客站点！
