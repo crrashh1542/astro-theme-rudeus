1ad8196fdb6d98e3cc2cf6e5c28fdd78e37e1f7c
crrashh1542 <1542@crrashh.com>
2026-03-02 14:54:41 +0800

    fix: 为Astro设置正确的配置
    Astro v6 需要在 src 目录就设置 content 的配置文件，故进行迁移。
    
    Signed-off-by: crrashh1542 <1542@crrashh.com>

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,13 +1,18 @@
-import { defineCollection, z } from 'astro:content'
+import { defineCollection } from 'astro:content'
+import { z } from 'astro/zod'
+import { glob } from 'astro/loaders'
 
 const archives = defineCollection({
-    type: 'content',
+    loader: glob({
+        pattern: '**/*.{md,mdx}',
+        base: './src/content/archives',
+    }),
     schema: z.object({
         title: z.string(),
         description: z.string().optional(),
         published: z.coerce.date(),
         updatedDate: z.coerce.date().optional(),
-        author: z.string().default('Anonymous'),
+        author: z.string().default('博主'),
         tags: z.array(z.string()).default([]),
         image: z.string().optional(),
     }),
