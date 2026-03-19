7ae423f3fe9d6cf1cfcc2c27acd39a0e59a879f0
crrashh1542 <1542@crrashh.com>
2026-03-19 21:37:26 +0800

    feat: 自定义博客多数属性
    - 可自定义侧边栏、版权信息与页脚
    - 为防止侵权事项，项目内置头像已换为自己画的自设
    
    Signed-off-by: crrashh1542 <1542@crrashh.com>

--- src/config.ts
+++ src/config.ts
@@ -5,6 +5,13 @@ export const siteConfig = {
     title: 'Rudeus 主题文档', // 站点标题
     subtitle: '一个基于 Astro 的简洁博客主题', // 站点副标题
     description: 'Rudeus 是一个基于 Astro 的简洁的博客主题', // SEO 站点描述
+    license: {
+        enabled: true, // 是否启用版权声明
+        name: 'CC BY-SA 4.0', // 版权类型
+        url: 'https://creativecommons.org/licenses/by-sa/4.0/', // 版权链接
+        prompt: '除特殊声明转载之外，本文由博主云萧原创且非 AI 生成内容，依据 [license] 许可协议授权，若需转载请注明出处及本声明。',
+        // 显示在页面的版权提示语，可使用 [license] 占位符来插入协议链接
+    },
 }
 
 // 导航栏链接配置
@@ -17,7 +24,7 @@ export const navConfig = [
 // 个人信息配置
 export const profileConfig = {
     name: '云萧是个咕咕怪', // 昵称
-    avatar: 'assets/avatar.jpg', // 头像路径
+    avatar: '/avatar.jpg', // 头像路径
     // 如果头像在外部链接，直接填写 URL
     // 如果在项目内，填写以 /public 为 base 的相对链接
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', // 简介
